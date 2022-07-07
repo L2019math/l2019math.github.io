9039e82b5cbf82665a235d1aeb12e65804cc6edc
L2019math <L2019math@163.com>
2022-07-07 14:42:08 +0800

    Site updated: 2022-07-07 14:42:04

--- sw.js
+++ sw.js
@@ -22,82 +22,56 @@ const handleerr = async(req, msg) => {
     <b>${msg}</b>`, { headers: { "content-type": "text/html; charset=utf-8" } })
 }
 let cdn = { //镜像列表
-        "gh": {
-            jsdelivr: {
-                "url": "https://cdn.jsdelivr.net/gh"
-            },
-            jsdelivr_fastly: {
-                "url": "https://fastly.jsdelivr.net/gh"
-            },
-            jsdelivr_gcore: {
-                "url": "https://gcore.jsdelivr.net/gh"
-            }
+    "gh": {
+        jsdelivr: {
+            "url": "https://cdn.jsdelivr.net/gh"
         },
-        "combine": {
-            jsdelivr: {
-                "url": "https://cdn.jsdelivr.net/combine"
-            },
-            jsdelivr_fastly: {
-                "url": "https://fastly.jsdelivr.net/combine"
-            },
-            jsdelivr_gcore: {
-                "url": "https://gcore.jsdelivr.net/combine"
-            }
+        jsdelivr_fastly: {
+            "url": "https://fastly.jsdelivr.net/gh"
         },
-        "npm": {
-            eleme: {
-                "url": "https://npm.elemecdn.com"
-            },
-            jsdelivr: {
-                "url": "https://cdn.jsdelivr.net/npm"
-            },
-            zhimg: {
-                "url": "https://unpkg.zhimg.com"
-            },
-            unpkg: {
-                "url": "https://unpkg.com"
-            },
-            bdstatic: {
-                "url": "https://code.bdstatic.com/npm"
-            },
-            tianli: {
-                "url": "https://cdn1.tianli0.top/npm"
-            },
-            sourcegcdn: {
-                "url": "https://npm.sourcegcdn.com/npm"
-            }
+        jsdelivr_gcore: {
+            "url": "https://gcore.jsdelivr.net/gh"
+        }
+    },
+    "combine": {
+        jsdelivr: {
+            "url": "https://cdn.jsdelivr.net/combine"
+        },
+        jsdelivr_fastly: {
+            "url": "https://fastly.jsdelivr.net/combine"
+        },
+        jsdelivr_gcore: {
+            "url": "https://gcore.jsdelivr.net/combine"
+        }
+    },
+    "npm": {
+        eleme: {
+            "url": "https://npm.elemecdn.com"
+        },
+        jsdelivr: {
+            "url": "https://cdn.jsdelivr.net/npm"
+        },
+        zhimg: {
+            "url": "https://unpkg.zhimg.com"
+        },
+        unpkg: {
+            "url": "https://unpkg.com"
+        },
+        bdstatic: {
+            "url": "https://code.bdstatic.com/npm"
+        },
+        tianli: {
+            "url": "https://cdn1.tianli0.top/npm"
+        },
+        sourcegcdn: {
+            "url": "https://npm.sourcegcdn.com/npm"
+        }
 
-        }
     }
-    //主控函数
-const handle = async function(req) {
-    const urlStr = req.url
-    const domain = (urlStr.split('/'))[2]
-    let urls = []
-    for (let i in cdn) {
-        for (let j in cdn[i]) {
-            if (domain == cdn[i][j].url.split('https://')[1].split('/')[0] && urlStr.match(cdn[i][j].url)) {
-                urls = []
-                for (let k in cdn[i]) {
-                    urls.push(urlStr.replace(cdn[i][j].url, cdn[i][k].url))
-                }
-                if (urlStr.indexOf('@latest/') > -1) {
-                    return lfetch(urls, urlStr)
-                } else {
-                    return caches.match(req).then(function(resp) {
-                        return resp || lfetch(urls, urlStr).then(function(res) {
-                            return caches.open(CACHE_NAME).then(function(cache) {
-                                cache.put(req, res.clone());
-                                return res;
-                            });
-                        });
-                    })
-                }
-            }
-        }
-    }
-    return fetch(req)
 }
+
+
+
 const lfetch = async(urls, url) => {
     let controller = new AbortController();
     const PauseProgress = async(res) => {
@@ -140,4 +114,88 @@ const lfetch = async(urls, url) => {
                 })
         })
     }))
+}
+
+const fullpath = (path) => {
+    path = path.split('?')[0].split('#')[0]
+    if (path.match(/\/$/)) {
+        path += 'index'
+    }
+    if (!path.match(/\.[a-zA-Z]+$/)) {
+        path += '.html'
+    }
+    return path
+}
+
+const generate_blog_urls = (packagename, blogversion, path) => {
+    const npmmirror = [
+        `https://unpkg.com/${packagename}@${blogversion}/public`,
+        `https://npm.elemecdn.com/${packagename}@${blogversion}/public`,
+        `https://cdn.jsdelivr.net/npm/${packagename}@${blogversion}/public`,
+        `https://npm.sourcegcdn.com/npm/${packagename}@${blogversion}/public`,
+        `https://cdn1.tianli0.top/npm/${packagename}@${blogversion}/public`
+    ]
+    for (var i in npmmirror) {
+        npmmirror[i] += path
+    }
+    return npmmirror
+}
+
+self.db = { //全局定义db,只要read和write,看不懂可以略过
+    read: (key, config) => {
+        if (!config) { config = { type: "text" } }
+        return new Promise((resolve, reject) => {
+            caches.open(CACHE_NAME).then(cache => {
+                cache.match(new Request(`https://LOCALCACHE/${encodeURIComponent(key)}`)).then(function(res) {
+                    if (!res) resolve(null)
+                    res.text().then(text => resolve(text))
+                }).catch(() => {
+                    resolve(null)
+                })
+            })
+        })
+    },
+    write: (key, value) => {
+        return new Promise((resolve, reject) => {
+            caches.open(CACHE_NAME).then(function(cache) {
+                cache.put(new Request(`https://LOCALCACHE/${encodeURIComponent(key)}`), new Response(value));
+                resolve()
+            }).catch(() => {
+                reject()
+            })
+        })
+    }
+}
+
+const set_newest_version = async(mirror) => { //改为最新版本写入数据库
+    return lfetch(mirror, mirror[0])
+        .then(res => res.json()) //JSON Parse
+        .then(async res => {
+            await db.write('blog_version', res.version) //写入
+            return;
+        })
+}
+
+setInterval(async() => {
+    await set_newest_version(mirror) //定时更新,一分钟一次
+}, 60 * 1000);
+
+setTimeout(async() => {
+    await set_newest_version(mirror) //打开五秒后更新,避免堵塞
+}, 5000)
+
+//主控函数
+const handle = async function(req) {
+    const urlStr = req.url
+    const urlObj = new URL(urlStr);
+    const urlPath = urlObj.pathname;
+    const domain = urlObj.hostname;
+    if (domain === "selfknow.cn") { //这里写你需要拦截的域名
+        //从这里开始处理
+        return lfetch( //若第一次没有,则使用初始版本1.1.4,或者你也可以改成latest(不推荐)
+                generate_blog_urls('chenyfan-blog', await db.read('blog_version') || '1.1.4', fullpath(urlPath)))
+            .then(res => res.arrayBuffer()) //arrayBuffer最科学也是最快的返回
+            .then(buffer => new Response(buffer, { headers: { "Content-Type": "text/html;charset=utf-8" } })) //重新定义header
+    }
+
 }
