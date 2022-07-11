08b16cfcacb07b4f0eac34cbd1f4104af2fa1e23
L2019math <L2019math@163.com>
2022-07-11 14:53:58 +0800

    Site updated: 2022-07-11 14:53:53

---
+++ sw.js
@@ -0,0 +1,158 @@
+const CACHE_NAME = 'ICDNCache';
+let cachelist = [];
+self.addEventListener('install', async function(installEvent) {
+    self.skipWaiting();
+    installEvent.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function(cache) {
+            console.log('Opened cache');
+            return cache.addAll(cachelist);
+        })
+    );
+});
+self.addEventListener('fetch', async event => {
+    try {
+        event.respondWith(handle(event.request))
+    } catch (msg) {
+        event.respondWith(handleerr(event.request, msg))
+    }
+});
+const handleerr = async(req, msg) => {
+    return new Response(`<h1>CDN分流器遇到了致命错误</h1>
+    <b>${msg}</b>`, { headers: { "content-type": "text/html; charset=utf-8" } })
+}
+let cdn = { //镜像列表
+    "gh": {
+        jsdelivr: {
+            "url": "https://cdn.jsdelivr.net/gh"
+        },
+        jsdelivr_fastly: {
+            "url": "https://fastly.jsdelivr.net/gh"
+        },
+        jsdelivr_gcore: {
+            "url": "https://gcore.jsdelivr.net/gh"
+        }
+    },
+    "combine": {
+        jsdelivr: {
+            "url": "https://cdn.jsdelivr.net/combine"
+        },
+        jsdelivr_fastly: {
+            "url": "https://fastly.jsdelivr.net/combine"
+        },
+        jsdelivr_gcore: {
+            "url": "https://gcore.jsdelivr.net/combine"
+        }
+    },
+    "npm": {
+        eleme: {
+            "url": "https://npm.elemecdn.com"
+        },
+        jsdelivr: {
+            "url": "https://cdn.jsdelivr.net/npm"
+        },
+        zhimg: {
+            "url": "https://unpkg.zhimg.com"
+        },
+        unpkg: {
+            "url": "https://unpkg.com"
+        },
+        bdstatic: {
+            "url": "https://code.bdstatic.com/npm"
+        },
+        tianli: {
+            "url": "https://cdn1.tianli0.top/npm"
+        },
+        sourcegcdn: {
+            "url": "https://npm.sourcegcdn.com/npm"
+        }
+
+    }
+}
+const lfetch = async(urls, url) => {
+    let controller = new AbortController();
+    const PauseProgress = async(res) => {
+        return new Response(await (res).arrayBuffer(), { status: res.status, headers: res.headers });
+    };
+    if (!Promise.any) {
+        Promise.any = function(promises) {
+            return new Promise((resolve, reject) => {
+                promises = Array.isArray(promises) ? promises : []
+                let len = promises.length
+                let errs = []
+                if (len === 0) return reject(new AggregateError('All promises were rejected'))
+                promises.forEach((promise) => {
+                    promise.then(value => {
+                        resolve(value)
+                    }, err => {
+                        len--
+                        errs.push(err)
+                        if (len === 0) {
+                            reject(new AggregateError(errs))
+                        }
+                    })
+                })
+            })
+        }
+    }
+    return Promise.any(urls.map(urls => {
+        return new Promise((resolve, reject) => {
+            fetch(urls, {
+                    signal: controller.signal
+                })
+                .then(PauseProgress)
+                .then(res => {
+                    if (res.status == 200) {
+                        controller.abort();
+                        resolve(res)
+                    } else {
+                        reject(res)
+                    }
+                })
+        })
+    }))
+}
+self.addEventListener('fetch', async event => {
+    event.respondWith(handle(event.request))
+});
+
+const fullpath = (path) => {
+    path = path.split('?')[0].split('#')[0]
+    if (path.match(/\/$/)) {
+        path += 'index'
+    }
+    if (!path.match(/\.[a-zA-Z]+$/)) {
+        path += '.html'
+    }
+    return path
+}
+
+const generate_blog_urls = (packagename, blogversion, path) => {
+    const npmmirror = [
+        `https://unpkg.com/${packagename}@${blogversion}/public`,
+        `https://npm.elemecdn.com/${packagename}@${blogversion}/public`,
+        `https://cdn.jsdelivr.net/npm/${packagename}@${blogversion}/public`,
+        `https://npm.sourcegcdn.com/npm/${packagename}@${blogversion}/public`,
+        `https://cdn1.tianli0.top/npm/${packagename}@${blogversion}/public`
+    ]
+    for (var i in npmmirror) {
+        npmmirror[i] += path
+    }
+    return npmmirror
+}
+const handle = async(req) => {
+    const urlStr = req.url
+    const urlObj = new URL(urlStr);
+    const urlPath = urlObj.pathname;
+    const domain = urlObj.hostname;
+    //从这里开始
+
+    if (domain === "www.selfknow.cn") { //这里写你需要拦截的域名
+        //从这里开始处理
+        return lfetch(generate_blog_urls('www.selfknow.cn', '1.1.9', fullpath(urlPath)))
+            .then(res => res.arrayBuffer()) //arrayBuffer最科学也是最快的返回
+            .then(buffer => new Response(buffer, { headers: { "Content-Type": "text/html;charset=utf-8" } })) //重新定义header
+    }
+
+    return fetch(req)
+}
